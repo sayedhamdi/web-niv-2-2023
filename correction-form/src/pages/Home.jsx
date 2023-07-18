75358f9f40902f28ea336dd2b7a68d80da862a3f
sayed <tkdsayed@gmail.com>
2023-07-18 18:27:50 +0100

    todos

--- correction-form/src/pages/Home.jsx
+++ correction-form/src/pages/Home.jsx
@@ -1,5 +1,10 @@
 import Form from "../components/Form"
+import  {useState} from "react"
 export default function Home(){
+    const [todos,setTodos] = useState([])
+    function addTodo(){
+
+    }
     return( 
         <div>
               <ul>
@@ -8,6 +13,11 @@ export default function Home(){
             </ul>
             <h1>Home page</h1>
             <Form/>
+            <ul>
+               {todos.length==0 ? 'no elements to show ' : todos.map(todo=><li>{todo}</li>)}
+            </ul>
+            <input type="text" />
+            <button onClick={()=>setTodos([...todos,"mohsen"])}>add</button>
         </div>
     )
 }
